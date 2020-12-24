40954f5ea0d626bbe74f7bb18568f63ce9d03754
GiGGioSo <gioele.pettarini@gmail.com>
2020-12-24 12:01:42 +0100

    Changed the game window name into "Kyrios - alla ricerca di se stesso".

--- Kyrios/src/main/Game.java
+++ Kyrios/src/main/Game.java
@@ -5,7 +5,7 @@ import javax.swing.JFrame;
 public class Game {
 
 	public static void main(String[] args) {
-		JFrame window = new JFrame("Kyrios");
+		JFrame window = new JFrame("Kyrios - alla ricerca di se stesso");
 		window.setContentPane(new GamePanel());
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window.setResizable(false);
